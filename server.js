0f49dffc00d61e583f87c242de3f5bfa614da5ce
Higor Chaves <higor_oliveira@outlook.com>
2019-06-10 21:29:05 -0400

    Correção retorno POST

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const redis = require('redis');
-
+// Para utilizar localmente
+// const redis = require('redis');
 // const client = redis.createClient();
+
+// Para deploy no Heroku
 var client = require('redis').createClient(process.env.REDIS_URL);
 
 app.use(express.json());
@@ -32,7 +34,9 @@ app.post('/new/:rota', (req, res) => {
             res.status(400).send(`Erro - ${err}`);
             return;
         }
-        res.status(203).end();
+        res.status(203).send({
+            "rota": `https://fake-api-back.herokuapp.com/api/${req.params.rota}`
+        });
     });
 });
 
